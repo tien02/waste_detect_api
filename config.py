18f16c6b057ead4172ded3056db63a6af28e6b7d
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-03-09 14:17:56 +0700

    auto download ckpt file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os, gdown
+
 IMG_SIZE = (640, 640)
 IOU_THRESHOLD = 0.4
 CONFIDENCE_THRESHOLD = 0.25
-CKPT_PATH = "weights/best.pt"
+CKPT_DRIVE_PATH = 'https://drive.google.com/file/d/1697rIEjlGrPKdv69GO_TgvyYhek7y1Qd/view?usp=share_link'
--- src.py
+++ src.py
@@ -3,6 +3,8 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from termcolor import colored
+import gdown
+import os
 import cv2 as cv
 
 def read_img(image_path: str, to_rgb:bool = True, resize:bool = False) -> np.array:
@@ -32,10 +34,23 @@ def test_read_show_img(test_img_path:str = "demo.jpg"):
     print(colored("=== PASS ===", "green"))
 
 def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=config.CKPT_PATH, _verbose=False)
+    ckpt_path = check_ckpt_file()
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=ckpt_path, _verbose=False)
     model.conf = config.CONFIDENCE_THRESHOLD
     model.iou = config.IOU_THRESHOLD
     return model
+
+def check_ckpt_file():
+    if not os.path.exists('weights'):
+        os.makedirs('weights')
+
+    if len(os.listdir('weights')) == 0:  
+        print(colored("Downloading best.pt to ./weights...", "green"))
+        url = 'https://drive.google.com/uc?/export=download&id=' + config.CKPT_DRIVE_PATH.split('/')[-2]
+        gdown.download(url, 'weights/best.pt', quiet=False)
+    else:
+        print(colored("Find existing ckpt file in ./weights", "blue"))
+    return "weights/best.pt"
 
 if __name__ == "__main__":
     test_read_show_img()
