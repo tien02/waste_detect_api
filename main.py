588226519cb8b0792d2a5c5bb8187ba1130e0604
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-03-10 21:47:52 +0700

    add jsonify response

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import os, shutil
-from fastapi import FastAPI, File, UploadFile, Query
+from fastapi import FastAPI, File, UploadFile, Query, Response
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from src import read_img, load_model
 
 model = load_model()
 app = FastAPI()
+
+origins = [
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    # "http://localhost",
+    # "http://localhost:8000",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
@@ -16,9 +33,9 @@ def root():
 async def detection(
     img_file:UploadFile = File(..., description="Upload image file"),
     bb_format: str = Query('xyxy',description="Return bounding box in the chosen format, can be 'xyxy' or 'xywh'"),
-    return_image: bool | None = Query(False, description="Whether return images or just bounding box coordinate"),
+    return_image: bool = Query(False, description="Whether return images or just bounding box coordinate"),
     ):
-
+    
     # read image
     img = read_img(image_path=img_file.file)
 
@@ -35,4 +52,5 @@ async def detection(
         prediction.save()
         return FileResponse('./runs/detect/exp/image0.jpg')
 
-    return json_result
+    # return json_result
+    return Response(json_result, media_type="application/json")
