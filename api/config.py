cf98a2b98e9327870468e556417074fd78d2ba75
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-06-02 15:28:16 +0700

    update model

--- api/config.py
+++ api/config.py
@@ -2,4 +2,4 @@ IMG_SIZE = (640, 640)
 IOU_THRESHOLD = 0.7
 CONFIDENCE_THRESHOLD = 0.25
 
-DRIVE_PATH = "https://drive.google.com/file/d/1049GTUJppo8oBBdmhzwEMHd0T10IgwsA/view?usp=sharing"
+DRIVE_PATH = "https://drive.google.com/file/d/10UrcWp6uOLN6y1XouWp2kqSiWXKWp7WW/view?usp=sharing"
